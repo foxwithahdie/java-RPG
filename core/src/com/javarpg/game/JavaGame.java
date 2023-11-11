363254f2485908d1130c4801eed398a1b05970cc
Ramzey Burdette <ramzey.burdette2005@gmail.com>
2023-11-11 17:43:35 -0500

    created build for new game

---
+++ core/src/com/javarpg/game/JavaGame.java
@@ -0,0 +1,32 @@
+package com.javarpg.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import org.w3c.dom.css.Rect;
+
+public class JavaGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0, 1);
+		batch.begin();
+		batch.draw(img, 100, 100);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
